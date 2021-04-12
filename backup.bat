ecd1be9588456b67eab89fec9c75cd57ec526105
Ilya Igonin <47140422+suspect47@users.noreply.github.com>
2021-04-12 19:05:48 +0300

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,5 +1,7 @@
 @echo off
 taskkill /IM 1cv8c.exe /F /T
 chcp 1251
+net use x: \\192.168.30.44\backup /user:admin Pa$$w0rd
 "C:\Program Files\WinRAR\WinRAR.exe" a -agYYYY-MM-DD_HH-MM -r -m5 \\192.168.30.44\backup\ D:\1C_base
 Forfiles -p \\192.168.30.44\backup\ -s -m *.rar -d -90 -c "cmd /c del /q @path"
+net use x: /delete
