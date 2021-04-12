ad1a32df5cd8f067ffd914341f41e690b4754e18
Ilya Igonin <47140422+suspect47@users.noreply.github.com>
2021-04-12 16:12:07 +0300

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,4 +1,5 @@
+@echo off
 taskkill /IM 1cv8c.exe /F /T
 chcp 1251
-"C:\Program Files\WinRAR\WinRAR.exe" a -agYYYY-MM-DD_HH-MM -r -m5 Z:\ D:\1C_base
-Forfiles -p Z:\ -s -m *.rar -d -90 -c "cmd /c del /q @path"
+"C:\Program Files\WinRAR\WinRAR.exe" a -agYYYY-MM-DD_HH-MM -r -m5 \\192.168.30.44\backup\ D:\1C_base
+Forfiles -p \\192.168.30.44\backup\ -s -m *.rar -d -90 -c "cmd /c del /q @path"
